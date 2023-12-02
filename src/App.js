d2b689b61fe9e101a9a7b7df2e57ea56ad576383
Ewa <zalewskaewa7@wp.pl>
2023-12-02 23:51:43 +0100

    add notifications

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.scss';
 import Header from "./components/Header"
+import Notification from './components/Notification';
 
 function App() {
   return (
     <div className="App">
       < Header />
+      <Notification /> 
     </div>
   );
 }
---
+++ src/components/Notification.js
@@ -0,0 +1,120 @@
+import React from 'react'
+import "./scss/notification.scss"
+import Mark from "./assets/images/avatar-mark-webber.webp"
+import Angela from "./assets/images/avatar-angela-gray.webp"
+import Jacob from "./assets/images/avatar-jacob-thompson.webp"
+import Rizky from "./assets/images/avatar-rizky-hasanuddin.webp"
+import Kimberly from "./assets/images/avatar-kimberly-smith.webp"
+import Nathan from "./assets/images/avatar-nathan-peterson.webp"
+import Anna from "./assets/images/avatar-anna-kim.webp"
+
+
+
+function Notification() {
+
+    const notifications= [
+        {
+            avatarSrc: Mark,
+            autor: "Mark Webber",
+            reaction: "reacted to your recent post",
+            postTitle: "My first tournament today!",
+            group: "",
+            message: "",
+            data: "1m ago",
+            ifRead: false,
+        },
+        {
+            avatarSrc: Angela,
+            autor: "Angela Gray",
+            reaction: "followed you",
+            postTitle: "",
+            group: "",
+            message: "",
+            data: "5m ago",
+            ifRead: false,
+
+        },
+        {
+            avatarSrc: Jacob,
+            autor: "Jacob Thompson",
+            reaction: "has joined your group",
+            postTitle: "",
+            group: "Chess Club",
+            message: "",
+            data: "1 day ago",
+            ifRead: false,
+
+        },
+        {  
+            avatarSrc: Rizky,
+            autor: "Rizky Hasanuddin",
+            reaction: "sent you a private message",
+            postTitle: "",
+            group: "",
+            message: "Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and I'm already having lots of fun and improving my game.",
+            data: "5 days ago",
+            ifRead: true,
+
+        },
+        {    
+            avatarSrc: Kimberly,
+            autor: "Kimberly Smith",
+            reaction: "commented on your picture",
+            postTitle: "",
+            group: "",
+            message: "",
+            data: "1 week ago",
+            ifRead: true,
+
+        },
+        {
+            avatarSrc: Nathan,
+            autor: "Nathan Peterson",
+            reaction: "reacted to your recent post",
+            postTitle: "5 end-game strategies to increase your win rate",
+            group: "",
+            message: "",
+            data: "2 weeks ago",
+            ifRead: true,
+
+        },
+        {   
+            avatarSrc: Anna,
+            autor: "Anna Kim",
+            reaction: "left the group",
+            postTitle: "",
+            group: "Chess Club",
+            message: "",
+            data: "2 weeks ago",
+            ifRead: true,
+
+        }
+    ]
+
+  return (
+    <div className="componentNotifications">
+        {
+        notifications.map((item, index) =>{
+            return(
+                <div className="notification" key={index}>
+                    <img src={item.avatarSrc} alt={item.title}/>
+                    <div className="content">
+                        <h2>{item.autor}</h2>
+                        <div className="reaction">{item.reaction}</div>
+                        <span className="postTitile">{item.postTitle}</span>
+                        <span className="group">{item.group}</span>
+                        <span className={item.ifRead ? "" : "notRead"}>{item.ifRead}</span>
+                        
+                        <div className="message">{item.message}</div>
+                        <div className="data">{item.data}</div>
+                    </div>
+                </div>
+            )
+        }
+        )
+        }
+        </div>
+  )
+}
+
+export default Notification
